2c4603b250677d77d04f7288d34a699c488c1d7a
DaNussi <daniel.nussbaum@htlstp.at>
2023-02-14 08:46:05 +0100

    First Commit

---
+++ src/test/java/net/nussi/springbootbasics/SpringBootBasicsApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nussi.springbootbasics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBasicsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
