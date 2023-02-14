a431e2c22a0dce552cdf6a19e46f1fc816e86a2d
DaNussi <daniel.nussbaum@htlstp.at>
2023-02-14 08:57:15 +0100

    First Commit

--- src/main/java/net/nussi/springbootbasics/api/stream/ApiController.java
+++ src/main/java/net/nussi/springbootbasics/api/stream/ApiController.java
@@ -1,12 +1,9 @@
 package net.nussi.springbootbasics.api.stream;
 
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.mvc.method.annotation.StreamingResponseBody;
 import reactor.core.publisher.Flux;
 
 import java.time.Duration;
@@ -18,7 +15,7 @@ public class ApiController {
 
     @GetMapping(value = "/data", produces = MediaType.APPLICATION_STREAM_JSON_VALUE)
     public Flux<Object> streamDataFlux() {
-
+        
 
         return Flux.interval(Duration.ofSeconds(1)).map(i -> "Data stream line - " + i );
     }
